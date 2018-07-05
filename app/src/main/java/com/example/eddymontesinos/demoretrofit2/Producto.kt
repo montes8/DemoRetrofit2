e07be4097bfb1b8506b77bb5de85f2c946b4083a
emontesinos812 <emontesinos812@gmail.com>
2018-07-05 08:30:44 -0300

    creando modelo de producto

---
+++ app/src/main/java/com/example/eddymontesinos/demoretrofit2/Producto.kt
@@ -0,0 +1,8 @@
+package com.example.eddymontesinos.demoretrofit2
+
+class Producto (val id: Int? = null,
+                val nombre: String,
+                val precio: Double,
+                val lote: Int,
+                val stock: Int,
+                val descripcion: String)
