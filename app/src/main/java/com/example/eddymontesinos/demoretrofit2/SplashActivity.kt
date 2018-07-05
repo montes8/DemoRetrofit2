65bd6c29c68103276690d85b0c0e3e86f7c302b7
emontesinos812 <emontesinos812@gmail.com>
2018-07-05 08:25:27 -0300

    creando splash

---
+++ app/src/main/java/com/example/eddymontesinos/demoretrofit2/SplashActivity.kt
@@ -0,0 +1,46 @@
+package com.example.eddymontesinos.demoretrofit2
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        val anima : Animation
+        val anima2 : Animation
+
+        anima = AnimationUtils.loadAnimation(this, R.anim.ani_arriba)
+        anima2 = AnimationUtils.loadAnimation(this,R.anim.ani_abajo)
+        once.animation=anima
+        doce.animation=anima2
+
+        llamarSplash()
+
+    }
+
+    fun llamarSplash (){
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep((3*1000).toLong())
+                    val i = Intent(baseContext, HomeActivity::class.java)
+                    startActivity(i)
+                    finish()
+                } catch (e: Exception) {
+
+
+                    e.printStackTrace()
+                }
+
+            }
+        }
+        background.start()
+    }
+}
