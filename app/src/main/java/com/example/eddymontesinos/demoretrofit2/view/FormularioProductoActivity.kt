da011842b589f9d46b176ad7f964448ee1b08fa9
emontesinos812 <emontesinos812@gmail.com>
2018-07-05 12:33:48 -0300

    creando diseño del formulario

--- app/src/main/java/com/example/eddymontesinos/demoretrofit2/view/FormularioProductoActivity.kt
+++ app/src/main/java/com/example/eddymontesinos/demoretrofit2/view/FormularioProductoActivity.kt
@@ -2,6 +2,7 @@ package com.example.eddymontesinos.demoretrofit2.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.eddymontesinos.demoretrofit2.R
 
 class FormularioProductoActivity : AppCompatActivity() {
 
